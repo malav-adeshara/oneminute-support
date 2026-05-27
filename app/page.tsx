7055cab345b5acfb23433a0e578663cdcc09725d
malav-adeshara <malavadeshara15@gmail.com>
2026-05-27 23:16:16 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/landing/nav';
+import React from 'react';
+
+export default function Page() {
+  return (
+    <main className='w-full flex flex-col relative z-10'>
+      <Navbar />
+    </main>
+  );
+}
