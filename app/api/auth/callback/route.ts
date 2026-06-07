548cae0861c22b4e7d2c7bef724e46d2ae22551b
malav-adeshara <malavadeshara15@gmail.com>
2026-06-07 16:14:13 +0530

    Started working on authentication using scalekit

---
+++ app/api/auth/callback/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import scalekit from "../../../../lib/scalekit";
+
+
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = req.nextUrl;
+    const code = searchParams.get("code");
+    const error = searchParams.get("error");
+    const error_description = searchParams.get("error_description");
+
+    if (error)
+        return NextResponse.json(
+            { error: error_description },
+            { status: 401 }
+        );
+
+    if (!code)
+        return NextResponse.json(
+            { error: "No code provided" },
+            { status: 400 }
+        );
+
+    try {
+        const redirectUri = process.env.SCALEKIT_REDIRECT_URI!;
+
+        const authResult = await scalekit.authenticateWithCode(code, redirectUri);
+
+        const { user, idToken } = authResult;
+
+        const claims = await scalekit.validateToken(idToken);
+
+        const organizationId =
+            (claims as any).organization_id ||
+            (claims as any).org_id ||
+            (claims as any).oid ||
+            null;
+
+        if (!organizationId) {
+            return NextResponse.json(
+                { error: "No organization id found in token claims" },
+                { status: 500 }
+            );
+        }
+
+
+    } catch (error) {
+
+    }
+}
